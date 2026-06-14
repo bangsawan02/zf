7b727839fb5cd6333c1ae1e1b4b4facbd7e2473f
bangsawan02 <telo.kuh@gmail.com>
2026-06-15 02:23:05 +0700

    Update zygisk and .github workflows at 2026-06-14T19:23:05.198Z

--- module/src/jni/Application.mk
+++ module/src/jni/Application.mk
@@ -1,4 +1,4 @@
-APP_ABI      := arm64-v8a armeabi-v7a
+APP_ABI      := arm64-v8a
 APP_CPPFLAGS := -std=c++17 -fno-exceptions -fno-rtti -fvisibility=hidden -fvisibility-inlines-hidden -Os -flto
 APP_LDFLAGS  := -Wl,--gc-sections -flto
 APP_STL      := none
